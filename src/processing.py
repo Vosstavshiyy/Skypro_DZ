3cc56ad04fe4f82d5ff7738088af41283eb33336
Vosstavshiy <vlad.chistiakov2003@gmail.com>
2025-10-08 21:41:51 +0300

    Функция sort_by_date готова

--- src/processing.py
+++ src/processing.py
@@ -1,30 +1,20 @@
 def filter_by_state(args: list[dict], state: str = 'EXECUTED') -> list[dict]:
     """
-    Фунция возврата списка словарей соответствующих значению ключа "state" (по умолчанию 'EXECUTED'):
+    Функция фильтрации словарей по значению ключа "state" (По умолчанию "EXECUTED")
 
     Пример:
     >>> filter_by_state([{state: 'EXECUTED'},{state: 'CANCELED'}], state='CANCELED')
-    Вывод:
-    "[{'state': 'CANCELED'}]"
+    "[{state: 'CANCELED'}]"
     """
     return [arg for arg in args if arg.get('state') == state]
 
 
-test_data = [
-    {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-    {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
-    {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-    {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}
-]
+def sort_by_date(args: list[dict], reverse: bool = True) -> list[dict]:
+    """
+    Функция сортировки по "date" (по умолчанию — убывание)
 
-print(filter_by_state(test_data))
-
-
-def sort_by_date():
-    pass
-
-# Примеры работы функции
-# # Выход функции (сортировка по убыванию, т. е. сначала самые последние операции)
-# [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'}, {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}, {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'}, {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}]
-# Пример входных данных для проверки функции
-# [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'}, {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}, {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'}, {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]
+    Пример:
+    >>> sort_by_date([{'date': '2020-03-01'}, {'date': '2019-04-02'}], reverse=False)
+    "[{'date': '2019-04-02'}, {'date': '2020-03-01'}]"
+    """
+    return sorted(args, key=lambda x: x['date'], reverse=reverse)
